c49274f482c8080844f403f3d1da534ea31ef155
alevarmun1 <alevarmun1@alum.us.es>
2024-12-07 15:38:59 +0100

    test(#23): Add more Locust tests for download all

--- app/modules/dataset/tests/locustfile.py
+++ app/modules/dataset/tests/locustfile.py
@@ -11,10 +11,41 @@ class DatasetBehavior(TaskSet):
     def dataset(self):
         response = self.client.get("/dataset/upload")
         get_csrf_token(response)
+    # Tests de Locust para probar la feature de Download All
     @task
-    def download_all(self):
+    def download_all_uvl(self):
         # Prueba la funcionalidad download_all en formato UVL
         response = self.client.get("/dataset/download_all", params={"format": "UVL"})
+        if response.status_code == 200:
+            print("Download all datasets successful.")
+        elif response.status_code == 404:
+            print("No datasets available for download.")
+        else:
+            print(f"Error in download all: {response.status_code}")
+    @task
+    def download_all_dimacs(self):
+        # Prueba la funcionalidad download_all en formato DIMACS
+        response = self.client.get("/dataset/download_all", params={"format": "DIMACS"})
+        if response.status_code == 200:
+            print("Download all datasets successful.")
+        elif response.status_code == 404:
+            print("No datasets available for download.")
+        else:
+            print(f"Error in download all: {response.status_code}")
+    @task
+    def download_all_glencoe(self):
+        # Prueba la funcionalidad download_all en formato GLENCOE
+        response = self.client.get("/dataset/download_all", params={"format": "GLENCOE"})
+        if response.status_code == 200:
+            print("Download all datasets successful.")
+        elif response.status_code == 404:
+            print("No datasets available for download.")
+        else:
+            print(f"Error in download all: {response.status_code}")
+    @task
+    def download_all_splot(self):
+        # Prueba la funcionalidad download_all en formato SPLOT
+        response = self.client.get("/dataset/download_all", params={"format": "SPLOT"})
         if response.status_code == 200:
             print("Download all datasets successful.")
         elif response.status_code == 404:
