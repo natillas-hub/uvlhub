70f4e1b3df302b3da543efb9606fea50ef65dc43
alevarmun1 <alevarmun1@alum.us.es>
2024-12-07 15:28:50 +0100

    test(#23): Add Locust tests for download all

--- app/modules/dataset/tests/locustfile.py
+++ app/modules/dataset/tests/locustfile.py
@@ -11,6 +11,16 @@ class DatasetBehavior(TaskSet):
     def dataset(self):
         response = self.client.get("/dataset/upload")
         get_csrf_token(response)
+    @task
+    def download_all(self):
+        # Prueba la funcionalidad download_all en formato UVL
+        response = self.client.get("/dataset/download_all", params={"format": "UVL"})
+        if response.status_code == 200:
+            print("Download all datasets successful.")
+        elif response.status_code == 404:
+            print("No datasets available for download.")
+        else:
+            print(f"Error in download all: {response.status_code}")
 
 
 class DatasetUser(HttpUser):
